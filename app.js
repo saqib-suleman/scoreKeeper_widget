d4989064ad0d47ffa94d58e7cdde3d6ae227a8cb
Saqib Suleman <saqibs3291@gmail.com>
2021-02-15 22:12:06 +0500

    add select and score logic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
+const resetButton = document.querySelector('#reset')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
+const winningScoreSelect = document.querySelector('#playto')
 
 let p1Score = 0;
 let p2Score = 0;
@@ -28,3 +30,17 @@ p2Button.addEventListener('click', function () {
     }
 })
 
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+}

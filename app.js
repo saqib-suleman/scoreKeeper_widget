113b00c4da73e1cc6461d0c3d9003e1928a14c82
Saqib Suleman <saqibs3291@gmail.com>
2021-02-15 21:55:25 +0500

    add player button logic

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
 
 let p1Score = 0;
+let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', function () {
-    if (p1Score !== winningScore) {
+    if (!isGameOver) {
         p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
-
 })
+
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
+})
+
